abfc536817cbb6f258e42e5d612a2e9269c7f7b7
Geoff Schmit <gcschmit@gmail.com>
2016-12-07 14:12:25 -0600

    finished ArrayListExample class

--- Chapter 7 Notes/ArrayListExample.java
+++ Chapter 7 Notes/ArrayListExample.java
@@ -20,6 +20,63 @@ public class ArrayListExample
         
         addValuesToList( list, 20 /* # of values */, 10 /* range of values */ );
         
+        System.out.println( list );
+        
+        removeEvenElements( list );
+        
+        System.out.println( list );
+        
+    }
+    
+    public static void removeEvenElements( ArrayList<Integer> list )
+    {
+        /*
+         * To get the number of elements in the list, invoke the size method
+         */
+        for( int i = 0; i < list.size(); i++ )
+        {
+            /*
+             * To get the value of an element in the list, invoke the get
+             *      method and pass the element's index
+             */
+            int value = list.get( i );
+            
+            // check if the value is even
+            if( value % 2 == 0 )
+            {
+                /*
+                 * To remove an element from the list, invoke the remove
+                 *      method and pass the element's index
+                 */
+                list.remove( i );
+                i--;
+            }
+        }
+    }
+    
+    public static void removeEvenElements2( ArrayList<Integer> list )
+    {
+        /*
+         * To get the number of elements in the list, invoke the size method
+         */
+        for( int i = list.size() - 1; i >= 0; i-- )
+        {
+            /*
+             * To get the value of an element in the list, invoke the get
+             *      method and pass the element's index
+             */
+            int value = list.get( i );
+            
+            // check if the value is even
+            if( value % 2 == 0 )
+            {
+                /*
+                 * To remove an element from the list, invoke the remove
+                 *      method and pass the element's index
+                 */
+                list.remove( i );
+            }
+        }
     }
     
     public static void addValuesToList( ArrayList<Integer> list,
